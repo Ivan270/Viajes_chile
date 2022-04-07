bc167fef3bea0af6f0805844e2163d2d2acfb695
1Verdugo <ivan270@gmail.com>
2022-04-07 11:08:22 -0400

    estilo navbar, jquery fx, boton placeholder borrado

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,17 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+// fade in - fade out Quienes Somos
+$(document).ready(function () {
+    $(".txt-fade").click(function () {
+        $(".title-fade1").fadeOut();
+        $(".title-fade2").fadeOut("slow");
+        $(".title-fade3").fadeOut(3000);
+    });
+    $(".txt-fade").click(function () {
+        $(".title-fade1").fadeIn();
+        $(".title-fade2").fadeIn("slow");
+        $(".title-fade3").fadeIn(3000);
+    });
+});
