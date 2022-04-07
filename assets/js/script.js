3ccb57f52bb6c0cc5c42cd0b53faff9a2ac47664
1Verdugo <ivan270@gmail.com>
2022-04-07 19:31:41 -0400

    indentacion y formato

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,29 @@
 // Tooltip
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
 
 // fade in - fade out Quienes Somos
 $(document).ready(function () {
-    $(".txt-fade").click(function () {
-        $(".title-fade1").fadeOut();
-        $(".title-fade2").fadeOut("slow");
-        $(".title-fade3").fadeOut(3000);
-    });
-    $(".txt-fade").click(function () {
-        $(".title-fade1").fadeIn();
-        $(".title-fade2").fadeIn("slow");
-        $(".title-fade3").fadeIn(3000);
-    });
+  $(".txt-fade").click(function () {
+    $(".title-fade1").fadeOut();
+    $(".title-fade2").fadeOut("slow");
+    $(".title-fade3").fadeOut(3000);
+  });
+  $(".txt-fade").click(function () {
+    $(".title-fade1").fadeIn();
+    $(".title-fade2").fadeIn("slow");
+    $(".title-fade3").fadeIn(3000);
+  });
 });
 
-$(window).scroll(function(){
-    if($("#nav-menu").offset().top > 56){
-        $("#nav-menu").addClass("bg-dark")
-    }else{
-        $("#nav-menu").removeClass("bg-dark")
-    }
-})
+$(window).scroll(function () {
+  if ($("#nav-menu").offset().top > 56) {
+    $("#nav-menu").addClass("bg-dark");
+  } else {
+    $("#nav-menu").removeClass("bg-dark");
+  }
+});
